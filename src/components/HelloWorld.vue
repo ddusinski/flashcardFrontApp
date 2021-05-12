5c0af1592db5ebe7693831b3511bcff5694e606a
ddusinski <dominik.dusinski@gmail.com>
2021-05-12 16:56:02 +0200

    fetching flashcard and rendering the question

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,7 +1,26 @@
 <template>
-  <h1>{{ msg }}
-    <input v-model="name">
-    <button v-on:click="sayHello(name)">My name is {{ name }}</button>
+  <h1>
+
+    <!--    <input v-model="name">-->
+    <!--    <button v-on:click="sayHello(name)">My name is {{ name }}</button>-->
+
+    <input v-model="flashCardId">
+    <button v-on:click="fetchDict(flashCardId)">choose flashcard</button>
+    {{ msg }}
+
+    <br>
+    <br>
+    <button v-on:click="drawFlashcard()">choose flashcard</button>
+    <br>
+
+
+    {{ flashcardContent['originalWord'] }}
+    <div v-for="(value) in flashcardContent['guessOptions']" :key="value">
+      <input type="radio" name="answer">{{ value }}
+    </div>
+    <br>
+    <button>Check answer</button>
+
   </h1>
 </template>
 
@@ -11,15 +30,31 @@ export default {
   data() {
     return {
       msg: '',
-      name:''
+      name: '',
+      flashCardId: '',
+      flashcardContent: []
     }
   },
-  methods:{
-    sayHello(myName){
-      fetch("/api/greeting?name="+myName)
+  methods: {
+    sayHello(myName) {
+      fetch("/api/greeting?name=" + myName)
           .then((response) => response.json())
           .then((data) => {
             this.msg = data.content
+          })
+    },
+    fetchDict(myFlashcardId) {
+      fetch("api/dict?id=" + myFlashcardId)
+          .then((response) => response.text())
+          .then((data) => {
+            this.msg = data
+          })
+    },
+    drawFlashcard() {
+      fetch("api/draw")
+          .then((response) => response.json())
+          .then((data) => {
+            this.flashcardContent = data
           })
     }
   },
