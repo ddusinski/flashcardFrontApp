426d5808f1a51a77bd15b5985ca53e1b925fb214
ddusinski <dominik.dusinski@gmail.com>
2021-05-13 14:52:19 +0200

    Checking result of flashcard draw

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,27 +1,53 @@
 <template>
-  <h1>
-
-    <!--    <input v-model="name">-->
-    <!--    <button v-on:click="sayHello(name)">My name is {{ name }}</button>-->
-
-    <input v-model="flashCardId">
-    <button v-on:click="fetchDict(flashCardId)">choose flashcard</button>
-    {{ msg }}
-
+  <h2>
+    Welcome to flashcard APP
+    <br>Frontend written in vue.js
+    <br>REST Backend written in SpringBoot
     <br>
     <br>
-    <button v-on:click="drawFlashcard()">choose flashcard</button>
-    <br>
+    <table>
+      <tr>
+        <td>
+          <button v-on:click="drawFlashcard()">Draw flashcard</button>
+          <br>
 
-
-    {{ flashcardContent['originalWord'] }}
-    <div v-for="(value) in flashcardContent['guessOptions']" :key="value">
-      <input type="radio" name="answer">{{ value }}
-    </div>
-    <br>
-    <button>Check answer</button>
-
-  </h1>
+          {{ flashcardContent['originalWord'] }} (id={{ flashcardContent['id'] }})
+          <div v-for="(answerValue) in flashcardContent['guessOptions']" :key="answerValue">
+            <input type="radio" name="answer" v-bind:value=answerValue v-model="pickedAnswer"><label>{{
+              answerValue
+            }}</label>
+          </div>
+        </td>
+      </tr>
+      <tr>
+        <td>
+          <button v-on:click="checkAnswer(flashcardContent['id'], pickedAnswer)">Check answer</button>
+          <br>
+          Result: {{ flashcardResult }}
+        </td>
+      </tr>
+      <tr>
+        <td>
+          <label>right answers count: {{ rightAnswerCount }} </label>
+          <br>
+          <label>wrong answers count: {{ wrongAnswerCount }} </label>
+          <br>
+          <button v-on:click="clearCounter()">Clear counter</button>
+        </td>
+      </tr>
+      <tr>
+        <td>
+          Here you may check the flashcard:
+          <br>
+          <input v-model="flashCardId" placeholder="provide flashcard id">
+          <br>
+          <button v-on:click="fetchDict(flashCardId)">Check flashcard</button>
+          <br>
+          {{ msg }}
+        </td>
+      </tr>
+    </table>
+  </h2>
 </template>
 
 <script>
@@ -32,17 +58,14 @@ export default {
       msg: '',
       name: '',
       flashCardId: '',
-      flashcardContent: []
+      flashcardContent: [],
+      pickedAnswer: '',
+      flashcardResult: false,
+      rightAnswerCount: 0,
+      wrongAnswerCount: 0
     }
   },
   methods: {
-    sayHello(myName) {
-      fetch("/api/greeting?name=" + myName)
-          .then((response) => response.json())
-          .then((data) => {
-            this.msg = data.content
-          })
-    },
     fetchDict(myFlashcardId) {
       fetch("api/dict?id=" + myFlashcardId)
           .then((response) => response.text())
@@ -51,20 +74,40 @@ export default {
           })
     },
     drawFlashcard() {
+      this.pickedAnswer = '';
       fetch("api/draw")
           .then((response) => response.json())
           .then((data) => {
             this.flashcardContent = data
           })
+    },
+    async checkAnswer(flashcardId, answer) {
+      if (answer) {
+        await fetch("api/answer?questionId=" + flashcardId + "&flashcardAnswer=" + answer)
+            .then((response) => response.text())
+            .then((data) => {
+              this.flashcardResult = (data === "true")
+            })
+        if (this.flashcardResult === true) {
+          this.rightAnswerCount++
+          this.drawFlashcard()
+        } else {
+          this.wrongAnswerCount++
+        }
+      }
+    },
+    clearCounter() {
+      this.wrongAnswerCount = 0;
+      this.rightAnswerCount = 0;
     }
   },
   mounted() {
-    fetch("/api/greeting")
-        .then((response) => response.json())
-        .then((data) => {
-          this.msg = data.content
-        })
+    this.drawFlashcard()
   }
-
 }
 </script>
+<style>
+table, th, td {
+  border: 3px solid black;
+}
+</style>
